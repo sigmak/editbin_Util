814da18d3dc23658e095c841ba3f9932ace5768d
sigmak <dwfree74@gmail.com>
2020-11-15 02:44:52 +0900

    v-0.2
    부족한 파일 보충용

---
+++ editbin_Util/editbin_Util/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: sigmak
+ * Date: 2019-09-22
+ * Time: 오전 12:39
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Windows.Forms;
+
+namespace editbin_Util
+{
+	/// <summary>
+	/// Class with program entry point.
+	/// </summary>
+	internal sealed class Program
+	{
+		/// <summary>
+		/// Program entry point.
+		/// </summary>
+		[STAThread]
+		private static void Main(string[] args)
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new Form1());
+		}
+		
+	}
+}
